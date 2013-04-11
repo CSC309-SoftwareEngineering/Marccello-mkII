b833e745099e21f15422dd88c6f2cc430d3b3d83
Nathaniel Butterworth <empollondefisica@gmail.com>
2013-04-11 13:56:06 -0400

    updated buildDB.bat ----- Needs to be tested

--- Marccello/Queries/buildDB.bat
+++ Marccello/Queries/buildDB.bat
@@ -1,28 +1,28 @@
 @echo off
 set exists=0
 set curVer=
-:run script to create Database
+::run script to create Database
 sqlcmd -S %computername%\SQLEXPRESS -E -i 001MarccelloCreate.sql
 
-:read version number from DB_Version.txt
+::read version number from DB_Version.txt
 for %%f in (*.txt) do (
 	if %%f==DB_Version.txt (
 		set exists=1
 		ECHO DB_Version.txt already exists
-		:Read in version number from DB_Version.txt
+		::Read in version number from DB_Version.txt
 		for /f "delims=" %%i in (DB_Version.txt) do call set curVer=%%i
 	)
 )
 ECHO Current database version = %curVer%
 
-:if DB_Version does not exist, create it and add 001.sql
+::if DB_Version does not exist, create it and add 001.sql
 if %exists%==0 (
-	ECHO Creating DB_Version.txt
+	ECHO Create: DB_Version.txt
 	ECHO 001.sql >> DB_Version.txt
+	for /f "delims=" %%i in (DB_Version.txt) do call set curVer=%%i
 )
 
-
-:run all .sql files in current directory
+::run all .sql files in current directory
 for %%f in (*.sql) do (
 	if not %%f==001MarccelloCreate.sql (
 		if %%f gtr %curVer% (
